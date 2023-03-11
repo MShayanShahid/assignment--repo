8a5fb48130f3e2e5e9ff992706453d3df73b226b
MShayanShahid <127333987+MShayanShahid@users.noreply.github.com>
2023-03-11 20:23:47 +0500

    Update index.js

--- Calculator (Switch)/index.js
+++ Calculator (Switch)/index.js
@@ -1,5 +1,5 @@
 function Calc(num1, num2) {
-    var choice = 2;
+    var choice = 2;     /can be any From (1-4)
     console.log("Select Operator \n +(Add) by Pressing 1 \n -(Minus) by Pressing 2 \n x(Mult) by Pressing 3\n /(Divide) by Pressing 4\n");
     switch (choice) {
         case 1:
