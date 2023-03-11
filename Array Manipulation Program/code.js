eba021e4c8801fc634eff53902052f6223042251
MShayanShahid <127333987+MShayanShahid@users.noreply.github.com>
2023-03-11 20:00:51 +0500

    Add files via upload

---
+++ Array Manipulation Program/code.js
@@ -0,0 +1,7 @@
+var Fruits = ["apple", "banana", "cherry", "date", "elderberry"];
+Fruits.push("mango");
+console.log(Fruits);
+Fruits.unshift("orange");
+console.log(Fruits);
+Fruits.splice(3, 3, "pineapple");
+console.log(Fruits);
---
+++ Array Manipulation Program/code.ts
@@ -0,0 +1,7 @@
+let Fruits:string[] =["apple", "banana", "cherry", "date", "elderberry"];
+Fruits.push("mango");
+console.log(Fruits);
+Fruits.unshift("orange");
+console.log(Fruits);
+Fruits.splice(3,3,"pineapple");
+console.log(Fruits);
