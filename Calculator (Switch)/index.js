adbf8975b4d8cc07df35c723ae1604a50fa0e303
MShayanShahid <127333987+MShayanShahid@users.noreply.github.com>
2023-03-11 20:21:25 +0500

    Add files via upload

---
+++ Calculator (Switch)/index.js
@@ -0,0 +1,21 @@
+function Calc(num1, num2) {
+    var choice = 2;
+    console.log("Select Operator \n +(Add) by Pressing 1 \n -(Minus) by Pressing 2 \n x(Mult) by Pressing 3\n /(Divide) by Pressing 4\n");
+    switch (choice) {
+        case 1:
+            console.log("Answer:\t" + (num1 + num2));
+            break;
+        case 2:
+            console.log("Answer:\t" + (num1 - num2));
+            break;
+        case 3:
+            console.log("Answer:\t" + (num1 * num2));
+            break;
+        case 4:
+            console.log("Answer:\t" + (num1 / num2));
+            break;
+        default:
+            console.log("Wrong Input");
+    }
+}
+Calc(2, 2);
---
+++ Calculator (Switch)/index.ts
@@ -0,0 +1,23 @@
+function Calc(num1:number,num2:number)
+{   
+    let choice:number=2;
+    console.log("Select Operator \n +(Add) by Pressing 1 \n -(Minus) by Pressing 2 \n x(Mult) by Pressing 3\n /(Divide) by Pressing 4\n");
+    switch(choice)
+    {
+        case 1:
+            console.log("Answer:\t"+(num1+num2));
+            break;
+        case 2:
+            console.log("Answer:\t"+(num1-num2));
+            break;
+        case 3:
+            console.log("Answer:\t"+(num1*num2));
+            break;
+        case 4:
+            console.log("Answer:\t"+(num1/num2));
+            break;
+        default:
+            console.log("Wrong Input");
+    }
+}
+Calc(2,2);
